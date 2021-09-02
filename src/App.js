b17d7c344f2a6a3aeddf7185adcae6661a2a590f
jessy <jessy@diilabs.co.kr>
2021-09-02 21:05:58 +0900

    3.2 첫 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-import React, {Component} from 'react';
+import MyComponent from "./MyComponent";
 
-class App extends Component {
-  render() {
-    const name = 'react'
-    return <div className="react">{name}</div>;
-  }
+const App = () => {
+  return <MyComponent />
 }
 export default App;
