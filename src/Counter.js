0af0b25983eaf44dc8a6a56feec8a3db8fc2801e
jessy <jessy@diilabs.co.kr>
2021-09-02 21:42:23 +0900

    3.4.1.4 this.setState가 끝난 후 특정 작업 실행하기

--- src/Counter.js
+++ src/Counter.js
@@ -1,26 +1,27 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 
 class Counter extends Component {
   state = {
     number: 0,
     fixedNumber: 0
   };
+
   render() {
-    const { number, fixedNumber } = this.state;
+    const {number, fixedNumber} = this.state;
     return (
       <div>
         <h1>{number}</h1>
         <h2>바뀌지 않는 값: {fixedNumber}</h2>
         <button
           onClick={() => {
-            this.setState(prevState => {
-              return {
-                number: prevState.number + 1
-              };
-            });
-            this.setState(prevState => ({
-                number: prevState.number + 1
-            }));
+            this.setState({
+                number: number + 1
+              },
+              () => {
+                console.log('방금 setState가 호출되었습니다.');
+                console.log(this.state);
+              }
+            );
           }}
         >
           +1
@@ -29,4 +30,5 @@ class Counter extends Component {
     );
   }
 }
+
 export default Counter;
