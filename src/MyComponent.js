adfc1c42d0bfde1e51ba4e59105efb9f0fb1878b
jessy <jessy@diilabs.co.kr>
2021-09-02 21:16:41 +0900

    3.3.6 propTypes를 통한 props 검증

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,5 +1,7 @@
+import PropTypes from 'prop-types';
+
 const MyComponent = props => {
-  const { name, children } = props;
+  const {name, children} = props;
   return (
     <div>
       안녕하세요, 제 이름은 {name}입니다.<br/>
@@ -10,5 +12,8 @@ const MyComponent = props => {
 }
 MyComponent.defaultProps = {
   name : '기본 이름'
+}
+MyComponent.propTypes = {
+  name: PropTypes.string
 }
 export default MyComponent;
